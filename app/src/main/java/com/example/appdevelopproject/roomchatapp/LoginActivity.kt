473d7dad7fcd84862f13f0c33c9f97bb03c8e1dd
imgaram <s21049@gsm.hs.kr>
2022-04-06 19:27:17 +0900

    android room chatting app 3: 로그인 예외처리, 화면 전환

--- app/src/main/java/com/example/appdevelopproject/roomchatapp/LoginActivity.kt
+++ app/src/main/java/com/example/appdevelopproject/roomchatapp/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.appdevelopproject.roomchatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.appdevelopproject.R
+import com.example.appdevelopproject.RoomChatActivity
 import com.example.appdevelopproject.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -13,5 +16,21 @@ class LoginActivity : AppCompatActivity() {
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.goChatBtn.setOnClickListener {
+            adduser()
+        }
+    }
+
+    private fun adduser() {
+        val id = binding.userId
+        val username = binding.userUsername
+
+        // 값이 비지 않은 경우
+        if (id.text.toString().isNotEmpty() && username.text.toString().isNotEmpty()) {
+            startActivity(Intent(this, RoomChatActivity::class.java))
+        } else {    // 빈 경우
+            Toast.makeText(this, "모두 입력 하세요", Toast.LENGTH_SHORT).show()
+        }
     }
 }
