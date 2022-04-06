afdc28c48e4ffcdac5d115aa37c7109a495d335d
imgaram <s21049@gsm.hs.kr>
2022-04-06 21:15:52 +0900

    android room chatting app 5: room 적용

--- app/src/main/java/com/example/appdevelopproject/roomchatapp/LoginActivity.kt
+++ app/src/main/java/com/example/appdevelopproject/roomchatapp/LoginActivity.kt
@@ -3,19 +3,25 @@ package com.example.appdevelopproject.roomchatapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.appdevelopproject.R
 import com.example.appdevelopproject.RoomChatActivity
 import com.example.appdevelopproject.databinding.ActivityLoginBinding
+import com.example.appdevelopproject.roomchatapp.db.UserDbBuilder
+import com.example.appdevelopproject.roomchatapp.entity.UserEntity
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    var userDb: UserDbBuilder? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        userDb = UserDbBuilder.getInstance(this)
 
         binding.goChatBtn.setOnClickListener {
             adduser()
@@ -23,11 +29,14 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun adduser() {
-        val id = binding.userId
-        val username = binding.userUsername
+        val id = binding.userId.text.toString()
+        val username = binding.userUsername.text.toString()
+        val userEntity = UserEntity(id, username)
 
         // 값이 비지 않은 경우
-        if (id.text.toString().isNotEmpty() && username.text.toString().isNotEmpty()) {
+        if (id.isNotEmpty() && username.isNotEmpty()) {
+            userDb?.userDao()?.insert(userEntity)
+            Log.d("TAG", "adduser db data: ${userDb?.userDao()?.getAll().toString()}")
             startActivity(Intent(this, RoomChatActivity::class.java))
         } else {    // 빈 경우
             Toast.makeText(this, "모두 입력 하세요", Toast.LENGTH_SHORT).show()
