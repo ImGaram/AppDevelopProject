40d0491f1d9c39d4ed166c63f2fd53fec6a0c66f
imgaram <s21049@gsm.hs.kr>
2022-04-11 15:44:06 +0900

    android room chatting app 9: 채팅 room entity

---
+++ app/src/main/java/com/example/appdevelopproject/roomchatapp/entity/ChatEntity.kt
@@ -0,0 +1,15 @@
+package com.example.appdevelopproject.roomchatapp.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Chat_table")
+class ChatEntity(
+    @PrimaryKey(autoGenerate = true) var uid: Long,
+    @ColumnInfo(name = "sendId") var sendId: String,        // 보낸 사람의 id
+    @ColumnInfo(name = "comment") var comment: String,      // 보낸 글
+    @ColumnInfo(name = "sendTime") var sendTime: String     // 보낸 시간
+) {
+    constructor(): this(0, "", "", "00:00")
+}
